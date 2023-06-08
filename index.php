461c773667ac147f3c3f67cf999b9d82b682db48
Dirk Freijters <stage@fyn.nl>
2023-06-08 12:50:11 +0200

    Added check for empty inputs

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION['lastnameError'])) $_SESSION['lastnameError'] = "";
 if (!isset($_SESSION['passwordError'])) $_SESSION['passwordError'] = "";
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
     <title>Sign up to my silly projects</title>
@@ -21,14 +21,14 @@ if (!isset($_SESSION['passwordError'])) $_SESSION['passwordError'] = "";
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
+    $conn = new PDO("mysql:host=dirkdev.com;dbname=projects", $_ENV['USER'], $_ENV['PASS']);
+
     if (isset($_GET['submit'])) {
-        ValidateForm();
+        ValidateForm($conn);
     }
 
-    function ValidateForm()
+    function ValidateForm($conn)
     {
-        $conn = new PDO("mysql:host=dirkdev.com;dbname=projects", $_ENV['USER'], $_ENV['PASS']);
-
         $sth = $conn->prepare("SELECT 1 FROM `projects`.`users` WHERE `username` = ?");
         $sth->execute([$_GET['username']]);
 
@@ -49,24 +49,51 @@ if (!isset($_SESSION['passwordError'])) $_SESSION['passwordError'] = "";
     }
     ?>
     <h1 class="text-center mt-4 mb-5">Sign up</h1>
-    <form class="position-relative d-flex flex-column text-center align-items-center w-50 mx-auto">
-        <input required autocomplete="off" id="username-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="text" name="username" placeholder="Username">
-        <?php if ($_SESSION['usernameError']) { ?>
-            <div id="username-error" class="text-danger text-start w-100 mw-40">
-                <?php
-                switch ($_SESSION['usernameError']) {
-                    case "alreadyExists":
-                        echo "A user with this username already exists! Choose another username";
-                        break;
-                }
-                ?>
-            </div>
+    <form autocomplete="off" class="position-relative d-flex flex-column text-center align-items-center w-50 mx-auto">
+        <input type="text" style="display:none"><input type="password" style="display:none"><!--Prevent Firefox from autocompleting -->
+        <input required id="username-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="text" name="username" placeholder="Username">
+        <div id="username-error" class="text-danger text-start w-100 mw-40" style="display: <?= (isset($_SESSION['usernameError'])) ? "block" : "none" ?>">
+            <?php
+            switch ($_SESSION['usernameError']) {
+                case "alreadyExists":
+                    echo "A user with the username you entered already exists! Choose another username";
+                    break;
+            }
+            ?>
+        </div>
         <?php
-            unset($_SESSION['usernameError']);
-        } ?>
-        <input required autocomplete="off" id="first-name-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="text" name="first-name" placeholder="First name">
-        <input required autocomplete="off" id="last-name-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="text" name="last-name" placeholder="Last name">
-        <input required autocomplete="off" id="password-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="password" name="password" placeholder="Password">
+        unset($_SESSION['usernameError']);
+        ?>
+        <input required id="firstname-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="text" name="first-name" placeholder="First name">
+        <div id="firstname-error" class="text-danger text-start w-100 mw-40" style="display: <?= (isset($_SESSION['usernameError'])) ? "block" : "none" ?>">
+            <?php
+            switch ($_SESSION['firstnameError']) {
+            }
+            ?>
+        </div>
+        <?php
+        unset($_SESSION['firstnameError']);
+        ?>
+        <input required id="lastname-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="text" name="last-name" placeholder="Last name">
+        <div id="lastname-error" class="text-danger text-start w-100 mw-40" style="display: <?= (isset($_SESSION['usernameError'])) ? "block" : "none" ?>">
+            <?php
+            switch ($_SESSION['lastnameError']) {
+            }
+            ?>
+        </div>
+        <?php
+        unset($_SESSION['lastnameError']);
+        ?>
+        <input required id="password-input-field" class="input-group-text form-control text-start p-2 mt-4 mw-40" type="password" name="password" placeholder="Password">
+        <div id="password-error" class="text-danger text-start w-100 mw-40" style="display: <?= (isset($_SESSION['usernameError'])) ? "block" : "none" ?>">
+            <?php
+            switch ($_SESSION['passwordError']) {
+            }
+            ?>
+        </div>
+        <?php
+        unset($_SESSION['passwordError']);
+        ?>
 
         <input type="submit" id="submit" name="submit" value="Sign up" class="input-group-submit p-2 px-4 mx-auto mt-3 mw-40 btn btn-primary cursor-pointer">
     </form>
--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 $("#username-input-field").on("input", () => {
     $("#username-error").css("display", "none");
 });
+
+let inputFields = [
+    {
+        input: $("#username-input-field"),
+        error: $("#username-error"),
+    },
+    {
+        input: $("#firstname-input-field"),
+        error: $("#firstname-error"),
+    },
+    {
+        input: $("#lastname-input-field"),
+        error: $("#lastname-error"),
+    },
+    {
+        input: $("#password-input-field"),
+        error: $("#password-error"),
+    },
+];
+
+for (let inputField of inputFields) {
+    console.log(inputField);
+    inputField.input.on("input", () => {
+        if (inputField.error.val() == "" &&
+            inputField.input.val() == "") {
+            inputField.error.css("display", "block");
+            inputField.error.text("This field shouldn't be empty!");
+        }
+    });
+}
